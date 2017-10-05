ec0a5ba1029681720e0033987eb6b226ddc674df
Unknown <dipalo.luigi@gmail.com>
2017-10-04 19:43:26 -0700

    add or subtract

--- Swift test/ViewController.swift
+++ Swift test/ViewController.swift
@@ -6,12 +6,33 @@ class ViewController: UIViewController {
 
     
     
-    var buttonCount = 0
+  //  var buttonCount = 0
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
+    
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        buttonCount = buttonCount + 1
+        let addition = additionSwitch.isOn
+        
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+            
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+        }
+        
+        
+        
+        
+        /*
+        buttonCount += 1
        
         print(buttonCount)
         
@@ -21,7 +42,7 @@ class ViewController: UIViewController {
             
             myLabel.text = "you hit it 15 times"
         }
-        
+        */
     }
     
     
